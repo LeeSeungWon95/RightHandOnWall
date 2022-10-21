46d9aaab8b47d0a0e3255c7d0795320ef68750c6
LEESEUNGWON\Leeseungwon <iseungwon239@gmail.com>
2022-10-21 18:12:58 +0900

    first commit

---
+++ RightHandOnWall/pch.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include "Types.h"
+
+using namespace std;
+
+struct Pos
+{
+	bool operator==(Pos& pos)
+	{
+		return x == pos.x && y == pos.y;
+	}
+	bool operator!=(Pos& pos)
+	{
+		return !(*this == pos);
+	}
+	Pos operator+(Pos& pos)
+	{
+		Pos ret;
+		ret.x = x + pos.x;
+		ret.y = y + pos.y;
+		return ret;
+	}
+	Pos operator-(Pos& pos)
+	{
+		Pos ret;
+		ret.x = x - pos.x;
+		ret.y = y - pos.y;
+		return ret;
+	}
+	Pos& operator+=(Pos& pos)
+	{
+		x += pos.x;
+		y += pos.y;
+		return *this;
+	}
+	Pos& operator-=(Pos& pos)
+	{
+		x -= pos.x;
+		y -= pos.y;
+		return *this;
+	}
+	int32 x = 0;
+	int32 y = 0;
+};
+
+enum Dir
+{
+	DIR_UP = 0,
+	DIR_LEFT = 1,
+	DIR_DOWN = 2,
+	DIR_RIGHT = 3,
+
+	DIR_COUNT = 4
+};
+
